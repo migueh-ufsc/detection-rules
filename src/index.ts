4debb18e1b83d0e1c2e9acee0023113bf866e59e
indistormborn <indicm@gmail.com>
2023-05-21 22:55:26 -0300

    uns fixzinho pras coisa rodar em paz

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,13 @@
 import { default as Database } from 'infra/database/Connection';
-import { default as Broker } from 'infra/message-broker/Manager';
 import { default as Server } from 'infra/server/Server';
-import { TestPublisher } from './infra/message-broker/publishers/TestPublisher';
+import { ProfileDataService } from 'services/ProfileDataService';
+import { GenerateProfileDataUseCase } from 'usecases/GenerateProfileDataUseCase';
 
 (async () => {
-  Promise.all([Database.init(), Broker.init(), Server.init()]).then(
-    async () => {
-      const test = new TestPublisher();
-      test.publish(
-        JSON.stringify({
-          word: 'mensgaem enviada',
-          isItTrue: false,
-          value: 654,
-        }),
-      );
-    },
-  );
+  await Promise.all([Database.init(), Server.init()]);
+  const usercase = new GenerateProfileDataUseCase(new ProfileDataService());
+  //bot
+  await usercase.execute({ id: '914048929070587905' });
+  // human
+  await usercase.execute({ id: '5727802' });
 })();
--- src/usecases/GenerateProfileDataUseCase.ts
+++ src/usecases/GenerateProfileDataUseCase.ts
@@ -1,26 +1,31 @@
 import { BaseUseCase } from 'contracts/usecases/BaseUseCase';
 import { ProfileData } from 'entities/ProfileData';
 import { ProfileDataService } from 'services/ProfileDataService';
-import { TwitterIntegrationService } from 'src/integrations/twitter-integration/TwitterIntegrationService';
+import { TwitterIntegrationService } from 'integrations/twitter-integration/TwitterIntegrationService';
 
 export class GenerateProfileDataUseCase implements BaseUseCase {
   constructor(private readonly profileDataService: ProfileDataService) {}
 
   async execute(input: { username?: string; id?: string }): Promise<void> {
-    const userData = await TwitterIntegrationService.getUserData(input);
+    try {
+      const userData = await TwitterIntegrationService.getUserData(input);
 
-    const profileData = new ProfileData({
-      name: userData.name,
-      username: userData.username,
-      description: userData.description,
-      location: userData.location,
-      nTweet: userData.nTweets,
-      nFollower: userData.nFollowers,
-      nFollowing: userData.nFollowing,
-      tweets: userData.sampleTimeline,
-      createdAt: userData.accountCreatedAt,
-    });
+      const profileData = new ProfileData({
+        name: userData.name,
+        username: userData.username,
+        description: userData.description,
+        location: userData.location,
+        nTweet: userData.nTweets,
+        nFollower: userData.nFollowers,
+        nFollowing: userData.nFollowing,
+        tweets: userData.sampleTimeline,
+        createdAt: userData.accountCreatedAt,
+      });
 
-    await this.profileDataService.create(profileData);
+      await this.profileDataService.create(profileData);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
