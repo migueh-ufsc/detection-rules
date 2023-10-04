44be5aec8cc432ff2f882e407ac90b7d3cc1ee6f
indistormborn <indicm@gmail.com>
2023-10-03 22:48:59 -0300

    mais uns fixes envolvendo deleção de test files

--- src/infra/server/Routes.ts
+++ src/infra/server/Routes.ts
@@ -1,7 +1,6 @@
 /** Arquivo pra cadastrar rotas caso exista um server */
 import { Express } from 'express';
-import { default as TestRouter } from './routes/TestRouter';
 
 export const initRoutes = (app: Express) => {
-  app.use('/test', TestRouter);
+  app.use('/test', () => 0);
 };
