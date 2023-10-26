0eff5588210ba9306faf7eeafd3043cf2575137a
indistormborn <indicm@gmail.com>
2023-10-26 00:37:07 -0300

    add router e rotas

--- src/infra/server/Routes.ts
+++ src/infra/server/Routes.ts
@@ -1,6 +1,7 @@
 /** Arquivo pra cadastrar rotas caso exista um server */
 import { Express } from 'express';
+import { TwitterRouter } from './routers/routers-list';
 
 export const initRoutes = (app: Express) => {
-  app.use('/test', () => 0);
+  app.use('/twitter', TwitterRouter);
 };
