b99bec88ef6796c225cd533b2cfc00bfd142a74c
Indiara C. Menegat <indicm@gmail.com>
2024-06-03 02:47:49 -0300

    add POST /twitter/analysis/

---
+++ src/controllers/CreateProfileAnalysis.ts
@@ -0,0 +1,38 @@
+import { HttpError } from 'common/errors/HttpError';
+import { BaseController } from 'contracts/controllers/BaseController';
+import { HttpRequest, HttpResponse } from 'contracts/server/Http';
+import { logger } from 'infra/logger';
+import { GenerateProfileAnalysisUseCase } from 'usecases/GenerateProfileAnalysisUseCase';
+
+export class CreateProfileAnalysis implements BaseController {
+  constructor(readonly useCase: GenerateProfileAnalysisUseCase) {}
+
+  async handle(request: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { params } = request;
+      const { username } = params;
+
+      if (!username)
+        throw new HttpError({
+          status: 400,
+          message: 'Para gerar dados de análise é necessario um "username"',
+        });
+
+      const analysis = await this.useCase.execute(username);
+
+      return {
+        status: 200,
+        body: analysis,
+      };
+    } catch (error) {
+      if (error.status) throw error;
+
+      logger.error(error);
+
+      throw new HttpError({
+        message: 'Falha ao gerar dados de análise',
+        status: 500,
+      });
+    }
+  }
+}
--- src/infra/server/routers/TwitterRouter.ts
+++ src/infra/server/routers/TwitterRouter.ts
@@ -3,6 +3,9 @@ import { requestHandlerMidd } from '../Middlewares';
 import { CreateProfileData } from 'controllers/CreateProfileData';
 import { GenerateProfileDataUseCase } from 'usecases/GenerateProfileDataUseCase';
 import { ProfileDataService } from 'services/ProfileDataService';
+import { CreateProfileAnalysis } from 'controllers/CreateProfileAnalysis';
+import { GenerateProfileAnalysisUseCase } from 'usecases/GenerateProfileAnalysisUseCase';
+import { ProfileAnalysisService } from 'services/ProfileAnalysisService';
 
 const router = Router();
 
@@ -11,6 +14,18 @@ router.post(
   requestHandlerMidd(
     new CreateProfileData(
       new GenerateProfileDataUseCase(new ProfileDataService()),
+    ),
+  ),
+);
+
+router.post(
+  '/analysis/:username',
+  requestHandlerMidd(
+    new CreateProfileAnalysis(
+      new GenerateProfileAnalysisUseCase(
+        new ProfileDataService(),
+        new ProfileAnalysisService(),
+      ),
     ),
   ),
 );
