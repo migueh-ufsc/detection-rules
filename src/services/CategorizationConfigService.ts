5d1fa0312fc0fa33826c3d42b1b7522aa8384109
Indiara C. Menegat <indicm@gmail.com>
2024-06-09 19:12:20 -0300

    faltou o service

---
+++ src/services/CategorizationConfigService.ts
@@ -0,0 +1,9 @@
+import { ICategorizationConfig } from "contracts/entities/ICategorizationConfig";
+import { BaseService } from "./BaseService";
+import { CategorizationConfigModel } from "infra/database/schemas/CategorizationConfigSchema";
+
+export class CategorizationConfigService extends BaseService<ICategorizationConfig>{
+  constructor() {
+    super(CategorizationConfigModel);
+  }
+}
