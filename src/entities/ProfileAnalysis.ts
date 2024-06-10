5111627bcfbce52a27d720cde9c51a31ef0f7e2b
Indiara C. Menegat <indicm@gmail.com>
2024-06-09 21:20:20 -0300

    cria generateAnalysis function e adiciona mais informações a ela, alem de add oslimites nos calculos de algumas regras

--- src/entities/ProfileAnalysis.ts
+++ src/entities/ProfileAnalysis.ts
@@ -3,88 +3,112 @@ import {
   IProfileAnalysis,
 } from 'contracts/entities/IProfileAnalysis';
 import { IProfileData } from 'contracts/entities/IProfileData';
-import { Config } from 'infra/config';
 import { distance } from 'fastest-levenshtein';
+import { CategorizationConfigService } from 'services/CategorizationConfigService';
 
 export class ProfileAnalysis implements IProfileAnalysis {
   _id: string;
   profileData: IProfileData;
   accountType?: AccountType;
   // Proporção de seguidos para seguidores
-  readonly followingToFollowerRatioScore?: number;
+  followingToFollowerRatioScore?: number;
   // Proporção de retweets entre os tweets da timeline
-  readonly retweetToTweetRatioScore?: number;
+  retweetToTweetRatioScore?: number;
   // Proporção de quantas menções únicas são feitas
-  readonly mentionsPerUserScore?: number;
+  mentionsPerUserScore?: number;
   // Média do tamanho dos tweets
-  readonly tweetSizeAvgScore?: number;
+  tweetSizeAvgScore?: number;
   // Idade da conta em dias
-  readonly accountAgeScore?: number;
+  accountAgeScore?: number;
   // Proporção de utilização de hashtags
-  readonly hashtagUsageScore?: number;
+  hashtagUsageScore?: number;
   // Quantidade de tweets em relação a idade da conta
-  readonly tweetCountToAccountAgeScore?: number;
+  tweetCountToAccountAgeScore?: number;
   // Média de tempo de postagem entre um tweet e outro
-  readonly similarityBetweenNameAndUsernameScore?: number;
+  similarityBetweenNameAndUsernameScore?: number;
   // Similaridade entre o nome do usuário e o username
-  readonly avgTimeBetweenPostsScore?: number;
+  avgTimeBetweenPostsScore?: number;
 
   constructor(props: IProfileAnalysis) {
     this.profileData = props.profileData;
     this.accountType = props.accountType;
-    this.followingToFollowerRatioScore =
-      this.calculateFollowingToFollowerRatio();
-    this.retweetToTweetRatioScore = this.calculateRetweetToTweetRatio();
-    this.mentionsPerUserScore = this.calculateUniqueMentionRatio();
-    this.tweetSizeAvgScore = Math.log1p(
-      this.profileData.timelineSampleUserTweetTextSizeAvg,
-    );
-    this.accountAgeScore = Math.log1p(props.profileData.accountAgeInDays);
-    this.hashtagUsageScore = this.calculateUniqueHashtagRatio();
-    this.tweetCountToAccountAgeScore = this.calculateTweetPerDay();
-    this.similarityBetweenNameAndUsernameScore =
-      this.calculateSimilarityBetweenNameAndUsername();
-    this.avgTimeBetweenPostsScore = this.calculateAverageTimeBetweenTweets(); // in seconds
   }
 
-  private calculateFollowingToFollowerRatio(): number {
+  async generateAnalysis(): Promise<void> {
+    const { timelineSampleUserTweetTextSizeAvg, accountAgeInDays } =
+      this.profileData;
+    const [config] = await new CategorizationConfigService().find({});
+    const { limits } = config;
+
+    // quanto mais proixmo de 0, mais humano
+    this.followingToFollowerRatioScore = this.calculateFollowingToFollowerRatio(
+      limits.followingToFollowerRatioScore,
+    );
+
+    // quanto mais proximo de 0, mais humano
+    this.retweetToTweetRatioScore = this.calculateRetweetToTweetRatio(
+      limits.retweetToTweetRatioScore,
+    );
+
+    // quanto mais proximo de 0, mais bot
+    this.mentionsPerUserScore = this.calculateUniqueMentionRatio();
+
+    // quanto mais proximo de 0, mais bot
+    this.tweetSizeAvgScore = Math.log1p(timelineSampleUserTweetTextSizeAvg);
+
+    // quanto mais proximo de 0, mais bot
+    this.accountAgeScore = Math.log1p(accountAgeInDays);
+
+    // quanto mais proximo de 0, mais bot
+    this.hashtagUsageScore = this.calculateUniqueHashtagRatio();
+
+    // quanto mais proximo de 0, mais human
+    this.tweetCountToAccountAgeScore = this.calculateTweetPerDay();
+
+    // quanto mais proximo de 0, mais bot
+    this.avgTimeBetweenPostsScore = this.calculateAverageTimeBetweenTweets(); // in seconds
+
+    // quanto mais proixmo de 0, mais human
+    this.similarityBetweenNameAndUsernameScore =
+      this.calculateSimilarityBetweenNameAndUsername();
+  }
+
+  private calculateFollowingToFollowerRatio(limit: number): number {
     const ratio =
       this.profileData.nFollower !== 0
         ? this.profileData.nFollowing / this.profileData.nFollower
-        : Config.ruleConfig.maxFFRatio; // Evita a divisão por zero
+        : limit; // Evita a divisão por zero
 
-    return Math.log1p(Math.min(ratio, Config.ruleConfig.maxFFRatio));
+    return Math.log1p(Math.min(ratio, limit));
   }
 
   private calculateUniqueHashtagRatio(): number | null {
-    if (this.profileData.timelineSampleFullSize === 0) return null;
+    if (this.profileData.timelineSampleFullSize === 0 || this.profileData.timelineSampleHashtagCount === 0) return null;
 
     const totalHashtags = this.profileData.timelineSampleHashtagCount;
     const uniqueHashtags = Object.keys(this.profileData.hashtags).length;
-
-    if (totalHashtags === 0) {
-      return 0; // para evitar divisão por zero
-    }
 
     return Math.log1p(uniqueHashtags / totalHashtags);
   }
 
   private calculateUniqueMentionRatio(): number | null {
-    if (this.profileData.timelineSampleFullSize === 0) return null; // nao considera essa heuristica pra timelinse vazias
+    if (this.profileData.timelineSampleFullSize === 0 || this.profileData.timelineSampleMentionCount === 0) return null; // não considera essa heurística para timelines vazias
 
     const totalMentions = this.profileData.timelineSampleMentionCount;
     const uniqueMentions = Object.keys(this.profileData.mentions).length;
-    if (totalMentions === 0) return 0;
 
     return Math.log1p(uniqueMentions / totalMentions);
   }
 
-  private calculateRetweetToTweetRatio(): number | null {
-    if (this.profileData.timelineSampleFullSize === 0) return null; // desconsidera estatistica se nao tiver tweets
-    return Math.log1p(
-      this.profileData.timelineSampleRetweetSize /
-        this.profileData.timelineSampleFullSize,
-    );
+  private calculateRetweetToTweetRatio(limit: number): number | null {
+    if (this.profileData.timelineSampleFullSize === 0) return null; // desconsidera estatística se não houver tweets
+
+    const ratio =
+      this.profileData.timelineSampleRetweetSize !== 0
+        ? this.profileData.timelineSampleRetweetSize /
+        this.profileData.timelineSampleFullSize
+        : limit;
+    return Math.log1p(Math.min(ratio, limit));
   }
 
   private calculateSimilarityBetweenNameAndUsername(): number {
@@ -94,7 +118,7 @@ export class ProfileAnalysis implements IProfileAnalysis {
   }
 
   private calculateTweetPerDay(): number | null {
-    if (this.profileData.timelineSampleFullSize === 0) return null; // desconsidera estatistica se nao tiver tweets
+    if (this.profileData.nTweet === 0) return null; // desconsidera estatística se não houver tweets
     return Math.log1p(
       this.profileData.nTweet / this.profileData.accountAgeInDays,
     );
@@ -112,7 +136,7 @@ export class ProfileAnalysis implements IProfileAnalysis {
     const totalDiff = sortedDates.reduce((acc, curr, i, arr) => {
       if (i === 0) return acc;
       return (
-        acc + (new Date(curr).getTime() - new Date(arr[i - 1]).getTime()) / 1000 // ms to s
+        acc + (new Date(curr).getTime() - new Date(arr[i - 1]).getTime()) / 1000 // ms para s
       );
     }, 0);
 
--- src/services/CategorizationConfigService.ts
+++ src/services/CategorizationConfigService.ts
@@ -1,8 +1,8 @@
-import { ICategorizationConfig } from "contracts/entities/ICategorizationConfig";
-import { BaseService } from "./BaseService";
-import { CategorizationConfigModel } from "infra/database/schemas/CategorizationConfigSchema";
+import { ICategorizationConfig } from 'contracts/entities/ICategorizationConfig';
+import { BaseService } from './BaseService';
+import { CategorizationConfigModel } from 'infra/database/schemas/CategorizationConfigSchema';
 
-export class CategorizationConfigService extends BaseService<ICategorizationConfig>{
+export class CategorizationConfigService extends BaseService<ICategorizationConfig> {
   constructor() {
     super(CategorizationConfigModel);
   }
