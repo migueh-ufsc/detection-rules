6c78a9d2863dd9f80b568163943f075d15a13475
Indiara C. Menegat <indicm@gmail.com>
2024-11-10 14:00:19 -0300

    remove rabbit

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -3,16 +3,8 @@ dotenv.config();
 
 export abstract class Config {
   static readonly mongoURI = process.env.MONGODB_URI;
-  static readonly rabbitURI = process.env.RABBITMQ_URI;
   static readonly serverPort = process.env.SERVER_PORT || 3000;
   static readonly logLevel = process.env.LOG_LEVEL || 'info';
-  static readonly exchanges = [
-    {
-      name: 'test',
-      type: 'topic',
-      routingKeys: ['teste.key'],
-    },
-  ];
   static readonly twitterIntegrationURL =
     process.env.TWITTER_INTEGRATION_API || '';
 
--- src/infra/message-broker/subscribers/index.ts
+++
@@ -1,6 +0,0 @@
-/**  Neste arquivo serão importados todos os subscribers criados e
- * será configurado para que ouçam uma determinada exchange/queue/routinKey*/
-
-export const initSubscribers = (): Promise<void>[] => {
-  return [];
-};
