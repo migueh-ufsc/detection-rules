a0ec01fd1dc3c43e5dfd173b8dfda3ce09226a04
Indiara C. Menegat <indicm@gmail.com>
2024-11-10 14:13:17 -0300

    usa body ao inves de parametros pra receber dados de usuario pra categorization

--- src/controllers/Categorize.ts
+++ src/controllers/Categorize.ts
@@ -3,16 +3,24 @@ import { BaseController } from 'contracts/controllers/BaseController';
 import { HttpRequest, HttpResponse } from 'contracts/server/Http';
 import { logger } from 'infra/logger';
 import { CategorizationUseCase } from 'usecases/CategorizationUseCase';
+interface CategorizeHttpRequest extends HttpRequest {
+  body: {
+    username?: string;
+    id?: string;
+    force?: boolean;
+  };
+}
 
 export class Categorize implements BaseController {
-  constructor(readonly useCase: CategorizationUseCase) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(readonly useCase: CategorizationUseCase) { }
 
-  async handle(request: HttpRequest): Promise<HttpResponse> {
+  async handle(request: CategorizeHttpRequest): Promise<HttpResponse> {
     try {
-      const { params } = request;
-      const { username, id } = params;
+      const { body } = request;
+      const { username, id, force } = body;
 
-      const analysis = await this.useCase.execute({ username, id });
+      const analysis = await this.useCase.execute({ username, id, force });
 
       return {
         status: 200,
